cebc84927a77331aefa42988d39e066df1c08073
Tejas Cousik <tcousik0@gmail.com>
2019-04-22 20:33:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,6 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+def key_for_min_value(hash)
+  big_num = 10000
+  if hash 
